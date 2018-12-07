9eb22cc21262f4c5c1405e1b3af5d774b15ee14e
HelloToMyGames127 <indiegamer127@gmail.com>
2018-12-07 10:49:40 +0530

    Working on airplane controller.

---
+++ Assets/AirPlane Physics/Code/Scripts/Controller/IP_Airplane_Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Vishal
+{
+    public class IP_Airplane_Controller : IP_BaseRigidbody_Controller
+    {
+
+        #region variables
+
+        #endregion
+
+        #region Buildin methods
+        protected override void HandlePhysics()
+        {
+            
+        }
+        #endregion
+    }
+}
+
---
+++ Assets/AirPlane Physics/Code/Scripts/Controller/IP_BaseRigidbody_Controller.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Vishal
+{
+    [RequireComponent(typeof(Rigidbody), typeof(AudioSource))]
+    public class IP_BaseRigidbody_Controller : MonoBehaviour
+    {
+        #region private variables
+        private Rigidbody rb;
+        private AudioSource aSource;
+        #endregion
+
+        #region Buildin methods
+        // Use this for initialization
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+            aSource = GetComponent<AudioSource>();
+            if (aSource)
+            {
+                aSource.playOnAwake = false;
+            }
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            if (rb)
+            {
+                HandlePhysics();
+            }
+        }
+        #endregion
+
+        #region Custom methods
+        protected virtual void HandlePhysics() { }
+        #endregion
+    }
+}
+
